2d4e8a95a0525ef72df371d7d66b5b98782f2042
xebi4i <xebi4i@ukr.net>
2014-01-18 00:32:59 +0200

    Add 5 players in db allways when db have nothing.

--- NBA/apps/NBA_players/views.py
+++ NBA/apps/NBA_players/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 
 from NBA.apps.NBA_players.models import Player
 
+def create_some_players():
+    '''
+    Create some players, who was see in /players
+    '''
+    players = [['Tim Duncan', 'San Antonio Spurs', 35], ['Jeff Green', 'Boston Celtics', 27], ['Kevin Durant', 'Oklahoma City Thunder', 25], ['James Jones', 'Miami Heat', 33], ['Dwyane Wade', 'Miami Heat', 32]]
+    if str(Player.objects.all()) == '[]':
+        for player in players:
+            p = Player(name=player[0], team=player[1], age=player[2])
+            p.save()
+
 def index(request):
+    create_some_players()
     players_list = Player.objects.all().order_by('-name')
     context = {'players_list': players_list}
     return render(request, 'NBA_players/index.html', context)
