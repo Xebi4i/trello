f622243e98a513f928c44bca106224fe330330bd
xebi4i <xebi4i@ukr.net>
2014-01-17 18:53:01 +0200

    Chanch structure of project. Now apps placed in apps-folder and some mirror changes.

--- NBA_players/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-from NBA_players.models import Player
-
-def index(request):
-    players_list = Player.objects.all().order_by('-name')
-    context = {'players_list': players_list}
-    return render(request, 'NBA_players/index.html', context)
--- base_site/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'base_site/index.html')
